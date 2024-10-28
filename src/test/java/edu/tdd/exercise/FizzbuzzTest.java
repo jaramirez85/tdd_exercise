7b6194ed9be243b7728b2f1d1f71718b2320f1c3
vieramirez <javier.ramirez@mercadolibre.com.co>
2024-10-28 17:26:07 -0500

    Fizzbuzz exercise description

---
+++ src/test/java/edu/tdd/exercise/FizzbuzzTest.java
@@ -0,0 +1,41 @@
+package edu.tdd.exercise;
+
+///# Ejercicio FizzBuzz
+///
+/// El ejercicio FizzBuzz es un clásico en entrevistas técnicas y consiste en lo siguiente:
+///
+/// ## Descripción
+///
+/// Escribe un programa que imprima los números del 1 al 100. Pero para múltiplos de 3 imprime "Fizz" en lugar del número y para los múltiplos de 5 imprime "Buzz". Para los números que son múltiplos de ambos 3 y 5, imprime "FizzBuzz".
+///
+/// ### Requisitos
+///
+/// - Imprime los números del 1 al 100.
+/// - Para los números que son múltiplos de 3, imprime `Fizz` en lugar del número.
+/// - Para los números que son múltiplos de 5, imprime `Buzz` en lugar del número.
+/// - Para los números que son múltiplos de ambos (de 3 y 5), imprime `FizzBuzz` en lugar del número.
+///
+/// ### Ejemplo de salida
+/// ```
+/// 1
+/// 2
+/// Fizz
+/// 4
+/// Buzz
+/// Fizz
+/// 7
+/// 8
+/// Fizz
+/// Buzz
+/// 11
+/// Fizz
+/// 13
+/// 14
+/// FizzBuzz
+/// 16
+/// ...
+/// ```
+public class FizzbuzzTest {
+
+
+}
