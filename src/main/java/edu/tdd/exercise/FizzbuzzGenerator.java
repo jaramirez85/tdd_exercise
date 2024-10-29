f6e80f90dc0228713c1731ba43cb060964ab46da
vieramirez <javier.ramirez@mercadolibre.com.co>
2024-10-28 19:49:49 -0500

    escenario 6 (Refactor 1)

--- src/main/java/edu/tdd/exercise/FizzbuzzGenerator.java
+++ src/main/java/edu/tdd/exercise/FizzbuzzGenerator.java
@@ -1,29 +1,26 @@
 package edu.tdd.exercise;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FizzbuzzGenerator {
 
     public List<String> generate(int number) {
-        if(number <= 0){
-            return new ArrayList<>();
+        if (number <= 0) {
+            return Collections.emptyList();
         }
 
         List<String> result = new ArrayList<>();
         for (int i = 1; i <= number; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 result.add("FizzBuzz");
-            }else if(i % 3 == 0){
+            } else if (i % 3 == 0) {
                 result.add("Fizz");
-            }else {
-                if(i % 5 == 0){
-                    result.add("Buzz");
-                } else if (i % 3 == 0 && i % 5 == 0) {
-                    result.add("FizzBuzz");
-                } else{
-                    result.add(String.valueOf(i));
-                }
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
             }
         }
         return result;
