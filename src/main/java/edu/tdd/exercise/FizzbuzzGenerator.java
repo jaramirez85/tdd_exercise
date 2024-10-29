a8ff454d30b01e4714ebdcf565f39a3394680034
vieramirez <javier.ramirez@mercadolibre.com.co>
2024-10-28 19:30:27 -0500

    escenario 1

---
+++ src/main/java/edu/tdd/exercise/FizzbuzzGenerator.java
@@ -0,0 +1,12 @@
+package edu.tdd.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzbuzzGenerator {
+
+    public List<String> generate(int number) {
+        return new ArrayList<>();
+    }
+
+}
--- src/test/java/edu/tdd/exercise/FizzbuzzTest.java
+++ src/test/java/edu/tdd/exercise/FizzbuzzTest.java
@@ -1,12 +1,21 @@
 package edu.tdd.exercise;
 
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 ///# Ejercicio FizzBuzz
 ///
 /// El ejercicio FizzBuzz es un clásico en entrevistas técnicas y consiste en lo siguiente:
 ///
 /// ## Descripción
 ///
-/// Escribe un programa que imprima los números del 1 al 100. Pero para múltiplos de 3 imprime "Fizz" en lugar del número y para los múltiplos de 5 imprime "Buzz". Para los números que son múltiplos de ambos 3 y 5, imprime "FizzBuzz".
+/// Escribe un programa que imprima los números del 1 a un número N (dónde N es mayor a cero).
+/// Pero para múltiplos de 3 imprime "Fizz" en lugar del número y para los múltiplos de 5 imprime "Buzz". Para los números que son múltiplos de ambos 3 y 5, imprime "FizzBuzz".
 ///
 /// ### Requisitos
 ///
@@ -37,5 +46,13 @@ package edu.tdd.exercise;
 /// ```
 public class FizzbuzzTest {
 
+    @Test
+    public void givenZeroThenReturnsEmptyList(){
+
+        FizzbuzzGenerator generator = new FizzbuzzGenerator();
+        List<String> result = generator.generate(0);
+        assertThat(result).isEqualTo(Collections.emptyList());
+
+    }
 
 }
